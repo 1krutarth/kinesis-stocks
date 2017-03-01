c3e07faa90213c6a70e39de391b110e56256834c
Krutarth Patel <krutarth40analytics@gmail.com>
2017-03-01 15:55:07 -0500

    stock value fetched from source

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-from bs4 import BeautifulSoup as bsoup
-import grequests
+# from bs4 import BeautifulSoup as bsoup
+# import grequests
 
-url = "https://www.google.com/finance?q=NYSE:MMM"
-html = requests.get()
-# stock value
-ux = [url,url]
-ureq = ( grequests.get(u, hooks={'response':print_text}) for u in ux )
-responses = grequests.map( ureq )
-def print_text( r, *args, **kwargs ):
-	print( r.url )
-	print( r.text )
+# url = "https://www.google.com/finance?q=NYSE:MMM"
+# html = requests.get()
+# # stock value
+# ux = [url,url]
+# ureq = ( grequests.get(u, hooks={'response':print_text}) for u in ux )
+# responses = grequests.map( ureq )
+# def print_text( r, *args, **kwargs ):
+# 	print( r.url )
+# 	print( r.text )
 
-data is dictionary
-a = cPickle.dumps(data).encode('base64','strict')
-cPickle.loads(a.decode('base64','strict') )
+# bx = soup.find_all( 'span', attrs={'class':'pr'} )
+
+# data is dictionary
+# a = cPickle.dumps(data).encode('base64','strict')
+# cPickle.loads(a.decode('base64','strict') )
 
 from ConfigParser import SafeConfigParser
-from src import CsvReader
+from src import CsvReader, Request
 
 def main():
 	parser = SafeConfigParser()
 	parser.read( "config.ini" )
 
+	capacity = parser.get( 'stocks', 'number' )
+	
 	folder = parser.get( 'csv', 'folder' )
 	csv_file = parser.get( 'csv', 'filename' )
-	csv = CsvReader( folder, csv_file )
+	csv = CsvReader( folder, csv_file, int(capacity) )
 	stocks = iter( csv.get_stocks() )
+
+	url = parser.get( 'stocks', 'url' )
+	request = Request( url, stocks, int(capacity) )
+	stock_info = request.get_url_info()
+
+	print stock_info
+
+if __name__ == '__main__':
+	main()
--- src/request.py
+++ src/request.py
@@ -1,5 +1,36 @@
+import copy
 import grequests
+from response import Response
 
 class Request:
-	def __init__( self ):
-		pass
+	def __init__( self, url, stocks, capacity ):
+		self._capacity = capacity
+		self._urls = [None]*self._capacity
+		self._url = url + '{}'
+		self._stocks = list(stocks)
+		self.info = [None]*self._capacity
+
+	def _generate_urls( self ):
+		return ( self._url.format(stock) for stock in self._stocks )
+
+	def _send_requests( self ):
+		self._urls = self._generate_urls()
+		self._req_objects = ( grequests.get(url) for url in self._urls )
+		return grequests.imap( self._req_objects )
+
+	def _generate_threads( self ):
+		self._threads = [None]*self._capacity
+		for i, response in enumerate(self._responses):
+			t = Response( args=(response, self.info, i, self._stocks[i]) )
+			self._threads[i] = t
+			t.start()
+
+		for t in self._threads:
+			t.join()
+
+		self._threads = None
+
+	def get_url_info( self ):
+		self._responses = self._send_requests()
+		self._generate_threads()
+		return copy.deepcopy(self.info)
